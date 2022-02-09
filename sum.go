08acef62fa450d49ccca10d37264b521b73ef219
Rafael Paulino <suporte.paulino@gmail.com>
2022-02-08 21:48:04 -0300

    fix: Adicionado parametros do docker, porem comentados

--- sum.go
+++ sum.go
@@ -10,6 +10,7 @@ func sum(a int, b int) int {
 	return a + b
 }
 
+/*
 func sub(a int, b int) int {
 	return a - b
 }
@@ -21,3 +22,4 @@ func times(a int, b int) int {
 func sumX(a, b int) int {
 	return a + b + a
 }
+*/
